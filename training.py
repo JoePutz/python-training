91e6851d9f58748bc1d91b2bcd277be65f54c4d0
Joseph Putz <josephputz89@gmail.com>
2022-11-22 11:08:47 -0600

    first lesson

---
+++ training.py
@@ -0,0 +1,25 @@
+msg = "Hello World!"
+print(msg)
+print('Joe Putz')
+
+print('o----')
+print(' ||||')
+print('*\n' * 10)
+
+price = 10
+rating = 4.9
+name = 'Joe'
+is_published = False
+# booleans are capitalized True or False not true or false, python is case sensative
+print(price)
+
+# John Smith variable exercise
+full_name = "John Smith"
+age = 20
+is_new = True
+
+name = input('What is your name? ')
+print('Hi ' + name)
+
+color = input('What is your favorite color? ')
+print(name + ' likes ' + color)
