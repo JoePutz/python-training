cfae126387a7ccaf86599e03ad88d31b7870d4e3
Joseph Putz <josephputz89@gmail.com>
2022-11-22 12:04:25 -0600

    lesson 1.5 lists and dictionaries

---
+++ training1.py
@@ -0,0 +1,47 @@
+x = 5
+y = 2
+x
+
+"Hello"
+
+3 * 3
+
+3 + 3 - (3 / 3)
+
+x = 8
+
+y = 3
+
+z = x - y
+
+a = x + y
+
+sound = "cluck"
+
+sound = "moo"
+
+quote = 'and she said, "Yum!"'
+
+quote2 = 'and I said, "Ew!'
+
+quote3 = "Euh, 'merrpppp'"
+
+# this is saying b is true if x is 8, since x = 8 is true above b is true so if I typed b it would just be true
+b = x == 8
+
+# c is true if x > 5, this is true
+c = x > 5
+
+# d is true if sound is moo, sound is moo so it should be true
+d = sound == "moo"
+
+# this should be false
+e = sound == "yuck"
+
+# shows we can link things with and, this should be false
+f = x == 8 and sound == "yuck"
+
+g = x == 5 and sound == "moo"
+
+
+
---
+++ training2.py
@@ -0,0 +1,54 @@
+# list seems like an array
+[1, 2, 3]
+["jane", "john", "alex"]
+names = ["jane", "john", "alex"]
+
+# how to get the first name in names?
+print(names[0])
+
+names[0] = "Jen"
+
+print(names[0])
+
+# this should show "true" since jen is in the list of names
+print("Jen" in names)
+
+# only letters and underscores for this title
+# this is called a dictionary, but it seems like object
+name_to_cookies = {"jane": 3, "john": 2}
+print(name_to_cookies["jane"])
+
+name_to_cookies["jane"] = 10
+
+
+numbers = [4, 5, 2]
+
+numbers2 = [4, 2, 7]
+
+strings = ["a", "b", "c"]
+
+booleans = [True]
+
+numbers[0]
+
+strings[2]
+
+# remember dictionary is {} list is [] just object and array really
+name_to_hungry = {"Jack": False, "Jane": True, "Alex": True}
+
+name_to_burgers = {"Emily": 1, "Jenny": 2, "Neel": 3}
+
+names_to_names = {"Jack": "Emily", "Jen": "Neel", "Alex": "Bob"}
+
+age_to_name = {14: "Bob", 15: "Alex", 20: "Jenny"}
+
+# this should equal False
+name_to_hungry["Jack"]
+
+# this should be 3
+name_to_burgers["Neel"]
+
+list_of_dictionaries = [{"a": 1}, {"b": 2}]
+
+strings_to_lists = {"Jack": [1, 2, 3]}
+
