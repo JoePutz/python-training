22ee2172b18233ccabe3368297c7f6b953a8f500
Joseph Putz <josephputz89@gmail.com>
2022-12-06 11:32:28 -0600

    finished lesson

--- training3.py
+++ training3.py
@@ -72,3 +72,65 @@ any(True, False, False)
 
 #This joins ths input together separated by / so it will be google.com/mail/hi
 "/".join(["google.com", "mail", "hi"])
+
+#so now we're building our functions
+def square (x):
+  return x*x
+#This is the syntax, like JS but no {} it's all in : and then 2 spaces for what's inside
+
+square(5) 
+#This will equal 25
+
+#This should go at the top of the file
+from urllib.request import urlopen
+
+def get_temperature(city):
+    url = "https://wttr.in/" + city + "?format=%t"
+    page = urlopen(url)
+    raw = page.read()
+    temp = raw.decomde("utf-8")
+    return temp
+
+city = input("City: ")
+temp = get_temperature(city)
+print(temp)
+
+
+def mul3(x):
+    return 3*x
+
+def sub3(x):
+    return x - 3
+
+def prod(x, y): 
+    return x * y
+
+def add(x, y): 
+    return x + y
+
+def prod(x, y, z): 
+    return x * y * z
+
+#This will return a true/false boolean 
+def gt3(x):
+    return x > 3
+
+def lt10(x):
+    return x < 10
+
+#Will return true only if both are true
+def both_gt_3(x, y):
+    return x > 3 and y > 3
+
+#takes a list, and sums the entire list, then multiplies the sum by 3
+def sum3(lst):
+    return sum(lst) * 3
+
+def sumv(dict):
+    return sum(dict.values())
+
+def is_jack_one(dict):
+    return dict["jack"] == 1
+
+def add3_jack(dict):
+    return dict["jack"] + 3
