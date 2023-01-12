5bccb83b1321771612b95d500581d668f8c5bf4d
Joseph Putz <josephputz89@gmail.com>
2023-01-12 00:24:45 -0600

    boolean conditionalsaand the like

--- training9.py
+++ training9.py
@@ -63,3 +63,100 @@ def weirdInput2():
         print('odd')
 
 
+#conditional statements
+#essentially if /then or whatever. in python this is all in booleans.
+
+hungry = True
+if hungry: 
+    print('Getting a sandwich.')
+
+#That's it. since hungry is true, running this code will produce "Getting a sandwich"
+
+lazy = False
+
+if lazy:
+    print('doing nothing')
+elif hungry:
+    print('getting sandwich')
+else: 
+    print('else statement')
+
+#This shows: getting sandwich
+#First it checks lazy. That is false. So goes to the next statement. Hungry is true, so goes there
+
+if None:
+    print('this')
+if '':
+    print('this')
+if 0:
+    print('this')
+if 'this':
+    print('that')
+#that
+#everyhting else goes to False
+
+var1 = 2
+var2 = 5
+
+if var1 == var2:
+    print('this')
+#does nothing
+
+if var1 < var2:
+    print('this')
+#this
+
+if True and True:
+    print('this')
+#this
+
+if True and False:
+    print('this')
+#this doens't print anything, becuase False is False
+
+if True or False:
+    print('this')
+#this
+# since it's 'or' True is True and that's enough
+
+if not True or False:
+    print('this')
+#nothing happens
+
+#'and' and 'or' returns what it represents
+'this' and ''
+#returns '' as that shows False
+
+'this' and 'that'
+#that, becuase tha'ts the last value
+
+'this' or 'that'
+#'this' as this is the first one that works
+
+'' or 'that'
+#'that' as taht's the one that actually returned True
+
+'fire' if True else 'ice'
+#'fire' 
+#this is a shorthand statement to do an either or. 
+
+def grade():
+    val = int(input('insert number: '))
+    if val >= 94:
+        return 'A'
+    if 80 <= val < 94:
+        return 'B'
+    if 70 <= val < 80:
+        return 'C'
+    if val < 70:
+        return 'failed'
+
+myvar = None
+
+'fire' if myvar else 'ice'
+#should be 'ice'
+
+'fire' if not myvar else 'ice'
+#should be 'fire'
+
+
