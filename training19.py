7ccb06339bd3933d6fb78d48dbeb02b32c1336fd
Joseph Putz <josephputz89@gmail.com>
2023-01-30 16:34:17 -0600

    project starter

---
+++ training19.py
@@ -0,0 +1,18 @@
+#this is going to transition to a project
+
+#but project will be using jsons and stuch.
+
+import json
+#imports jsons stuff
+
+import random
+
+mylist = [1, 2, 3, 4]
+random.shuffle(mylist)
+
+print(mylist)
+
+import os
+#os library
+
+os.system('clear')
