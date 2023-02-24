97779491fb9fcf47bf4ce650e0f0b35e4171fe27
Joseph Putz <josephputz89@gmail.com>
2023-02-24 11:24:04 -0600

    tuples and sets

--- training20.py
+++ training20.py
@@ -60,3 +60,64 @@ for index, course in enumerate(courses, start=1):
     print(index, course)
 #we can access index and value with enumerate function, returns both index and value
 #start=1 is a start value, which would essentially ignore what's on position 0. 
+
+course_str = ' - '.join(courses)
+#This creates a single string of teh values with - between them
+new_list = course_str.split(' - ')
+#Returns to initial list
+
+print(course_str)
+print(new_list)
+
+
+#Tuples
+
+#Can't modify tuples. immutable
+
+list_1 = ['History', 'Math', 'Physics', 'CompSci']
+list_2 = list_1
+#These are the same object.
+#Changing either of them changes both of them.
+
+tuple_1 = ('History', 'Math', 'Physics', 'CompSci')
+tuple_2 = tuple_1
+
+#tuple_1[0] = 'Art'
+#This will get ERROR: TypeError: 'tuple' object does not support item assignment
+
+#Sets
+#Unordered and have no duplicates
+
+cs_courses = {'History', 'Math', 'Physics', 'Compsci'}
+#If I print this, it might show up in the wrong order.
+
+
+cs_courses = {'History', 'Math', 'Physics', 'Compsci', 'Math'}
+#If I print this, then it will remove the second Math
+
+print ('Math' in cs_courses)
+#Prints: True
+
+art_courses = {'History', 'Math', 'Art', 'Design'}
+
+#Test if something is in either.
+
+print(cs_courses.intersection(art_courses))
+#{'History', 'Math'}
+
+print(cs_courses.difference(art_courses))
+#{'Physics', 'Compsci'}
+#It's specifically the ones just in cs_courses
+
+print(cs_courses.union(art_courses))
+#Unique values from both sets
+
+#Empty codes
+empty_list = []
+empty_list = list()
+
+empty_tuple = ()
+empty_tuple = tuple()
+
+empty_set = set()
+#empty_set = {} creates a Dictionary! Not a Set. Funky.
